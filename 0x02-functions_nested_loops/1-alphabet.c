0437f19c11848d52b1513e21b07fed021fc9c703
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-03-21 12:36:47 +0000

    1-alphabet.c program's added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
