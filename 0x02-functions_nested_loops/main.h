f71cc31e1de97c014a882adcbb021d55b387451a
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-03-21 12:48:31 +0000

    1-alphabet.c program's added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,22 @@
 #include <unistd.h>
 
+#ifndef PRINT_ALPHABET
+#define PRINT_ALPHABET
+void print_alphabet();
+#endif
+
 int _putchar(char c);
+
+/**
+ * print_alphabet - prints all alphabet in lower case
+ */
+void print_alphabet()
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+}
