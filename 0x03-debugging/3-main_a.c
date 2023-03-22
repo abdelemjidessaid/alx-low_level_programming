4137448df37e18e5498765948ed75e4d4d10b8f3
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-03-22 13:58:26 +0000

    Uploading 3-print_remaining_days.c file

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -9,19 +9,19 @@
 
 int main(void)
 {
-    int month;
-    int day;
-    int year;
+	int month;
+	int day;
+	int year;
 
-    month = 4;
-    day = 01;
-    year = 1997;
+	month = 4;
+	day = 01;
+	year = 1997;
 
-    printf("Date: %02d/%02d/%04d\n", month, day, year);
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-    day = convert_day(month, day);
+	day = convert_day(month, day);
 
-    print_remaining_days(month, day, year);
+	print_remaining_days(month, day, year);
 
 	month = 02;
 	day = 29;
@@ -33,6 +33,66 @@ int main(void)
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
+	
+	month = 01;
+	day = 01;
+	year = 2001;
+	/* 01/01/2001 */
 
+	printf("Date: %02d/%02d/%02d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	
+	
+	month = 12;
+	day = 01;
+	year = 2001;
+	/* 01/01/2001 */
+
+	printf("Date: %02d/%02d/%02d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	
+	
+	month = 12;
+	day = 31;
+	year = 2001;
+	/* 01/01/2001 */
+
+	printf("Date: %02d/%02d/%02d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	
+	
+	month = 1;
+	day = 00;
+	year = 2001;
+	/* 01/01/2001 */
+
+	printf("Date: %02d/%02d/%02d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	
+	
+	month = 12;
+	day = 30;
+	year = 2001;
+	/* 01/01/2001 */
+
+	printf("Date: %02d/%02d/%02d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	
+	
     return (0);
 }
