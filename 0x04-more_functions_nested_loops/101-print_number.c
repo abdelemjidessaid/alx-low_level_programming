7d7666416d252a5d097247d925936dcece574b00
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-03-23 11:05:41 +0000

    Updating 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,44 +8,33 @@
 
 void print_number(int n)
 {
+	int d, k;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		n *= -1;
+	}
+	d = n / 10, k = d / 10;
 
-		if (n <= 10)
-		{
-			_putchar(((n * -1) / 10) + 48);
-			_putchar(((n * -1) % 10) + 48);
-		}
-		else
-			_putchar(((n * -1) % 10) + 48);
+	if (n < 10)
+		_putchar('0' + n);
+	else if (n >= 10 && n < 100)
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 100 && n <= 1000)
+	{
+		_putchar((d / 10) + 48);
+		_putchar((d % 10) + 48);
+		_putchar((n % 10) + 48);
 	}
 	else
 	{
-		if (n < 10)
-			_putchar('0' + n);
-		else if (n >= 10 && n < 100)
-		{
-			_putchar((n / 10) + 48);
-			_putchar((n % 10) + 48);
-		}
-		else if (n >= 100 && n <= 1000)
-		{
-			int d = n / 10;
-
-			_putchar((d / 10) + 48);
-			_putchar((d % 10) + 48);
-			_putchar((n % 10) + 48);
-		}
-		else
-		{
-			int d = n / 10;
-			int k = d / 10;
-
-			_putchar((k / 10) + 48);
-			_putchar((k % 10) + 48);
-			_putchar((d % 10) + 48);
-			_putchar((n % 10) + 48);
-		}
+		_putchar((k / 10) + 48);
+		_putchar((k % 10) + 48);
+		_putchar((d % 10) + 48);
+		_putchar((n % 10) + 48);
 	}
 }
