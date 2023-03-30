eeadf80c0f155b537fafc5d37759ca794d9f945f
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-03-30 00:30:37 +0000

    Fixing errors on 101-print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,16 +10,21 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
-		n = -n;
+		num = -n;
 		_putchar('-');
 	}
-	if (n / 10 != 0)
+	else
+		num = n;
+
+	if (num / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
 
-	_putchar((n % 10) + 48);
+	_putchar((num % 10) + 48);
 
 }
