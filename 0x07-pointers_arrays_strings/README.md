649f259c8092bcd01423658bb412009f79e323db
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-03 13:22:06 +0000

    the file 0-memset.c uploaded.

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# 0x07-pointers_arrays_strings
+
+### In this project we will learn more about ***Arrays, Pointers & Strings***
+
+> - **0-memset.c** This function fills memory with contants byte.
+>
+> - ****
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+#ifndef _PUTCHAR /* start */
+#define _PUTCHAR
+int _putchar(int c);
+#endif /* end */
+
+#ifndef _MEMSET /* start */
+#define _MEMSET
+char *_memset(char *s, char b, unsigned int n);
+#endif /* end */
+
+
