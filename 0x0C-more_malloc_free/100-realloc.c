cc99e559592e8c8c6366aad6418d19d9a2d00ab3
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-13 17:09:35 +0000

    100-realloc.c uploaded.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (old_size == new_size)
 		return (ptr);
-	if (new_size > old_size || ptr == NULL)
+	if (new_size > old_size)
+		p = malloc(new_size);
+	if (ptr == NULL && new_size > 0)
 		p = malloc(new_size);
 	if (new_size == 0 && ptr != NULL)
 	{
