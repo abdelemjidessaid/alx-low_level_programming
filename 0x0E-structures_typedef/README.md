1e01992b1d6ae3486721b11d5435c949a7733eb6
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-17 13:36:37 +0000

    dog.h uploaded.

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+### 0x0E-structures_typedef
+
+##### In this project we will learn **struct** & **typedef** in C.
+
+> - **dog.h** Header file that contains a **dog struct**.
+>
+>
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - struct to create dog objects.
+ * @name: charsequence that contains the dog name.
+ * @age: float number that contains the dog age.
+ * @owner: charsequence that contains the dog owner's name.
+ *
+ * Description: dog struct is a new data type that is uses to define new
+ * dog object that contains dog's name, age and owner name.
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
