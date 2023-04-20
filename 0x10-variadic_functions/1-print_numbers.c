9af07471efad786d24ad629c39429fd907af7fbc
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-20 13:05:28 +0000

    1-print_numbers.c uploaded.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,8 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
-	if (n == 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(ap, int);
