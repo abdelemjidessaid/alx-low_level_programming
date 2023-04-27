16af5b23373567eb8c3ab6a0a0c98d62583660de
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-27 13:06:28 +0100

    Files of task 0 uploaded.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a linked list.
+ * @h: pointer to the head node.
+ * Return: the number of node that is printed.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t printed = 0;
+	const list_t *ptr = h;
+	char *data;
+	int len;
+
+	while(ptr)
+	{
+		printed++;
+		data = ptr->str;
+		len = ptr->len;
+		if (!data)
+		{
+			data = "(nil)";
+			len = 0;
+		}
+		printf("[%u] %s\n", len, data);
+		ptr = ptr->next;
+	}
+	return (printed);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+### 0x12-singly_linked_lists
+
+##### In this project we will learn the ___Singly Linked List___, and we have 7 tasks on it.
+
+> - **0-print_list.c** this file contains a function that prints all elements stored in a node.
