d83c3665c880980f49038b7e57732a4ea326fe0c
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-04-29 15:30:57 +0100

    Error of task 9 fixed, files uploaded

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	node = malloc(sizeof(listint_t));
-	if (!node || *head == NULL)
+	if (!node)
 		return (NULL);
 	node->n = n;
 	node->next = NULL;
@@ -35,9 +35,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (!current && idx == i)
+	if (!current && i == idx)
 	{
-		current = node;
+		prev->next = node;
 		return (node);
 	}
 	return (NULL);
