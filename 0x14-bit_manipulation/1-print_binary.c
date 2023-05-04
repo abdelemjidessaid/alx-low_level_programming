3c68d9a426257375f7d889c01710f350634720ab
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-05-04 16:12:38 +0100

    Files of task 1 uploaded

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,31 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int i = 1, flag = 1;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (i < n)
-		i <<= 1;
-	while (i > 0)
-	{
-		if (!(n & i) && flag)
-		{
-			flag = 0;
-			i >>= 1;
-			continue;
-		}
-		else
-			flag = 0;
-
-		if (n & i)
-			_putchar('1');
-		else
-			if (!flag)
-				_putchar('0');
-		i >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
