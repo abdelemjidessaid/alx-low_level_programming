56ba424a3d2d81654c3804732f86837a6bcde692
abdelemjid essaid <abdelemjidessaid@gmail.com>
2023-05-08 20:08:50 +0100

    Files of task 2 uploaded

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,13 +8,13 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len;
+	int fd, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (text_content)
-		for (; text_content[len] ;)
+		for (len = 0; text_content[len] ;)
 			len++;
 	if (write(fd, text_content, len) == -1)
 		return (-1);
