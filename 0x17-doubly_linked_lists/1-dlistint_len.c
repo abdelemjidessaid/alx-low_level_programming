891e651ca5198ce33c52c1a65d3ad42268008aca
Abdelemjid Essaid <abdelemjidessaid@gmial.com>
2023-06-15 10:55:57 +0100

    files of task 1 pushed up.

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - function that returns the number of node in a given
+ * doubly linked list.
+ * @h: pointer to the head of the given doubly linked list.
+ * Return: number of nodes.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
