d6a098c96e569c0d428cf877b2102745c1437a10
Abdelemjid Essaid <abdelemjidessaid@gmial.com>
2023-06-15 15:30:08 +0100

    files of task 7 are pushed up.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,13 +42,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		else
 		{
-			if (prev)
-			{
-				cur = add_dnodeint_end(&prev, n);
+			cur = add_dnodeint_end(&prev, n);
+			if (!prev)
 				*h = cur;
-			}
-			else
-				cur = add_dnodeint_end(&prev, n);
 		}
 		return (cur);
 	}
