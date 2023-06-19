f05b379624f0ceb55d589f5e825b32ccd50330ce
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-06-19 18:21:01 +0100

    files of task 100 -> pushed up.

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,55 @@
+
+/**
+ * add - function that adds two numbers to each other and return the result.
+ * @a: number one.
+ * @b: number two.
+ * Return: the result of addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mul - function that multiplies two number by each other and returns result.
+ * @a: number one.
+ * @b: number two.
+ * Return: the result of multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * sub - function that do subtraction of two numbers and returns the result.
+ * @a: number one.
+ * @b: number two.
+ * Return: the result of subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - function that divid two number by each others and returns a result.
+ * @a: number one.
+ * @b: number two.
+ * Return: the result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that returns the rest of division.
+ * @a: number one.
+ * @b: number two.
+ * Return: the result of modulo.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
