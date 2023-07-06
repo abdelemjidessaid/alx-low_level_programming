479725f0a95a08e47bedb20234af942f45d4a234
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-06 17:11:44 +0100

    Files of task 3 are created and pushed up.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds new an element to the hash table.
+ * @ht: pointer to the hash table.
+ * @key: pointer to the key.
+ * @value: pointer to the value.
+ * Return: 1 if add success, 0 otherwise.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *head, *new;
+
+	if (ht == NULL)
+		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
+	head = ht->array[index];
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	if (head == NULL)
+	{
+		ht->array[index] = new;
+		return (1);
+	}
+
+	new->next = head;
+	head = new;
+	return (1);
+}
