c7559311f851f74dc073993e0d48531e9dba1a8f
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-07 11:08:36 +0100

    0-hash_table_create.c -> updated.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,22 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
 
-	if (size <= 0)
-	{
+	if (size == 0)
 		return (NULL);
-	}
-	if (table == NULL)
-	{
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
 		return (NULL);
-	}
+
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
+	table->array = calloc(size, sizeof(hash_node_t));
+	if (!(table->array))
 	{
 		free(table);
 		return (NULL);
 	}
+
 	return (table);
 }
