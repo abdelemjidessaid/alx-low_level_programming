e4e99bd53bdc4fa96069fc00a069af9682ce22ed
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-07 11:18:54 +0100

    1-djb2.c -> updated, errors fixed.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,19 +2,20 @@
 
 /**
  * hash_djb2 - this is the hash function.
+ * @key: string of key that should converted to number.
  * Return: the index.
 */
-unsigned long int hash_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *key)
 {
-    unsigned long int hash;
-    int c, i = 0;
+	unsigned long int hash;
+	int c, i = 0;
 
-    hash = 5381;
-	c = str[i];
-    while (str[i])
-    {
-        hash = ((hash << 5) + hash) + c;
-		c = str[++i];
-    }
-    return (hash);
+	hash = 5381;
+	c = key[i];
+	while (key[i])
+	{
+		hash = ((hash << 5) + hash) + c;
+		c = key[++i];
+	}
+	return (hash);
 }
