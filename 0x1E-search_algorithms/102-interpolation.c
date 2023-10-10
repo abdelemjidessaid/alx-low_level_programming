61e80da513c11887ce32f2d39af28c0b61caadea
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-10 14:48:09 +0100

    Interpolation Search Algorithm is created.

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+
+/**
+ * interpolation_search - function that looks for an element in an array
+ * using the Interpolation Search Algorithm.
+ * @array: the list of elements which is array of numbers.
+ * @size: the number of elements.
+ * @value: the element we looking for.
+ * Return: the first index of element found, -1 otherwise.
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t l = 0, r = size - 1, i;
+
+	if (!array || size == 0)
+		return (-1);
+
+	while (l <= r)
+	{
+		i = l + ((r - l) / (array[r] - array[l])) * (value - array[l]);
+		if (i >= size)
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		else if (array[i] > value)
+			r = i;
+		else
+			l = i;
+	}
+
+	return (-1);
+}
