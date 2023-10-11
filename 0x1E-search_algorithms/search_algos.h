f8e639ce2c6c776798c14e5fa6926723ad7b490b
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-10-11 11:36:04 +0100

    Jump List Search Algorithm is created.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,25 @@
 #include <math.h>
 
 
+/*================= Structures ===================*/
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
 /*================= Prototypes ===================*/
 
 int linear_search(int *array, size_t size, int value);
@@ -16,6 +35,10 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
 
 
 #endif /* _SEARCH_ALGOS_H_ */
